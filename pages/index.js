ffa999caae862ecf48a1b44f24ba9b17a3f16df2
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-12-15 19:58:02 -0700

    Change landing font size.

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,13 @@ export default function Home() {
       </Head>
       <Box align="center" height={{ min: "100%" }} margin="large">
         <Box width="large" pad="medium" align="center">
-          <Heading>Small Minds</Heading>
+          <Heading size="large">Small Minds</Heading>
           <Box height="medium" width={{ max: "medium" }}>
             <Image fit="cover" src="/pictures/small-minds-round-min.png" />
           </Box>
-          <Paragraph>Embedded &amp; Full Stack Development</Paragraph>
+          <Paragraph size="large">
+            Embedded &amp; Full Stack Development
+          </Paragraph>
         </Box>
       </Box>
     </Box>
