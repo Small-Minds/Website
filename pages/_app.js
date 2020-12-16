595ea89a0bfeae2c209f81ccc79ed69b8b6a4864
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-12-15 17:35:58 -0700

    Make landing/menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { Anchor, Grommet, Header, Nav, Box, Menu } from "grommet";
+import "../styles/globals.css";
+import { theme } from "../styles/theme.js";
+
+const headerStyle = { position: "sticky", top: 0 };
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Grommet theme={theme}>
+      <Header background="primary" pad="small" style={headerStyle}>
+        <Box direction="row" pad="small">
+          <Anchor href="/" label="Small Minds" color="primary" />
+        </Box>
+        <Nav direction="row" pad="small">
+          <Menu
+            label="Menu"
+            items={[
+              { label: "Articles", href: "#" },
+              { label: "Contact", href: "#" },
+              { label: "Members", href: "#" },
+              { label: "GitHub", href: "https://github.com/small-minds" },
+            ]}
+          />
+        </Nav>
+      </Header>
+      <Component {...pageProps} />
+    </Grommet>
+  );
 }
 
-export default MyApp
+export default MyApp;
